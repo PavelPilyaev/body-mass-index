6c5618cc15385fa7b8fefb5163edbb3b7afeaeb2
Pavel Pilayev <4ernomorec@bk.ru>
2023-04-05 18:48:46 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public double calculate(double height, double weight) {
+        double result = (weight / (height * height));
+        return result;
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        // height - Рост в метрах
+        // weight - Вес в килограммах
+
+        System.out.println("Индекс массы тела:");
+        System.out.println(service.calculate(1.55, 65));
+
+    }
+}
+
